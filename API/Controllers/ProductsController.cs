9fc6c837acce916337f33a07f09c990e281c20ca
Martin Dragnev <mddragnev@gmail.com>
2022-05-01 21:01:51 +0300

    Add filtering by product type and product brand

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -30,9 +30,10 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort)
+        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
+            string sort, int? brandId, int? typeId)
         {
-            ProductsWithTypesAndBrandsSpecification spec = new(sort);
+            ProductsWithTypesAndBrandsSpecification spec = new(sort, brandId, typeId);
             var products = await _productsRepo.ListAsync(spec);
             return Ok(
                 _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
