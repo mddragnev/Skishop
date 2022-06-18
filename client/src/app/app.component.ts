fe9f825820fc0465f35fc5b01ca0412779f144da
Martin Dragnev <mddragnev@gmail.com>
2022-06-18 17:22:28 +0300

    Make calls between api and client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Skishop';
+export class AppComponent implements OnInit {
+  public title = 'Skishop';
+  public products: IProduct[];
+
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50')
+      .subscribe({
+        next: (response: IPagination) => {
+          this.products = response.data;
+        },
+        error: (error: any) => {
+          console.log(error);
+        }
+      });    
+  }
 }
